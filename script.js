79fa2230c5aa1345db6a6c08b317fb7e516314f0
Spencer Wood <spencercwood@gmail.com>
2017-02-07 16:05:01 -0700

    Add working, mostly unstyled version

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  var original = $('#original');
+  var result = $('#result');
+  $('#submit-button').on('click', convertTextToYoda);
+
+  function convertTextToYoda() {
+    var baseUrl = 'https://yoda.p.mashape.com/yoda?sentence=';
+    result.val('Loading...');
+    $.ajax({
+      url: baseUrl + encodeURIComponent(original.val()),
+      headers: {
+        'X-Mashape-Key': '8FhxKKgu73mshc8ztz3NjKzkp2Y5p1NgrkqjsnggzDfyJ6dGYn'
+      },
+      success: function (response) {
+        result.val(response);
+      }
+    });
+  }
+});
